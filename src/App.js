8d602e780ef59772baf278af17830b816bea9847
Shreekar Ganapati Hegde <shreekarhegde9@gmail.com>
2020-11-04 14:01:38 +0530

    Created search box component and added search functionality

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
 import React, { Component } from 'react';
-import './App.css';
+import { robots } from './robots';
+import CardList from './CardList/CardList';
+import SearchBox from './SearchBox/SearchBox';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-      </div>
-    );
-  }
+	constructor() {
+		super();
+		this.state = {
+			searchField: '',
+			robots: robots,
+		};
+	}
+
+	onSearchChange = (event) => {
+		console.log('event', event);
+		this.setState({ searchField: event.target.value });
+	};
+
+	render() {
+		const filteredRobots = this.state.robots.filter((robot) => {
+			return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase());
+		});
+		return (
+			<div>
+				<h1>Robo Friends</h1>
+				<div>
+					<SearchBox searchChange={this.onSearchChange} />
+					<CardList robots={filteredRobots} />
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,13 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import 'tachyons';
-import {robots} from './robots'
-import CardList from './CardList/CardList';
+import App from './App';
 
-ReactDOM.render(
-  <div>
-    <CardList robots={robots}/>
-  </div>
-  ,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
